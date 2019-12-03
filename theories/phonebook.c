975664e289dee98ea5f999ed049f86875f409ffd
minhdq99hp <minhdq99hp@gmail.com>
2019-12-03 13:49:14 +0700

    update

--- theories/phonebook.c
+++ theories/phonebook.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define INITIAL_SIZE 10
 #define INCREMENTAL_SIZE 5
@@ -25,7 +26,9 @@ PhoneBook createPhoneBook(){
 }
 
 void dropPhoneBook(PhoneBook *book){
-    free(book->entries);
+    for(int i=0; i<book->total; i++){
+        free(book->entries + i * sizeof(PhoneEntry));
+    }
     free(book);
 }
 
@@ -33,8 +36,32 @@ void addPhoneNumber(char *name, long number, PhoneBook *book){
     int foundIndex = -1;
 
     for(int i=0; i<book->total; i++){
-        if(book->entries+i)
+        if(strcmp((book->entries+i*sizeof(PhoneEntry))->name, name) == 0){
+            foundIndex = i;
+            (book->entries+i*sizeof(PhoneEntry))->number = number;
+            break;
+        }
     }
+
+    if(foundIndex == -1){
+        if(book->total < book->size){
+            strcpy((book->entries + total)->name, name);
+            (book->entries + total)->number = number;
+        }
+        else{ // dynamic allocate
+            book->size += INCREMENTAL_SIZE;
+            PhoneEntry *newEntries = (PhoneEntry*)malloc(sizeof(PhoneEntry) * book->size);
+            memcpy(newEntries, entries, sizeof(PhoneBook) * (book->size - INCREMENTAL_SIZE));
+            free(book->entries);
+            book->entries = newEntries;
+        }
+        total++;
+    }
+}
+
+PhoneEntry *getPhoneNumber(char *name, PhoneBook book){
+
+    return NULL;
 }
 
 int main(){
